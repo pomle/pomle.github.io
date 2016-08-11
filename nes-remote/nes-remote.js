7f37bcd6a15007dcbb35e8e789873fcf2b2e4f5b
Pontus Persson <pom@spotify.com>
2016-08-11 15:42:13 +0200

    Update nes-remote to branch alternate-touch.

--- nes-remote/nes-remote.js
+++ nes-remote/nes-remote.js
@@ -6,38 +6,85 @@ window.addEventListener('load', function() {
     'touchend': 'keyup',
   };
 
-  function handleKey(event) {
-    const keyName = event.target.id;
-    if (!keyName) {
-      return;
-    }
-    const payload = {
-      key: keyName,
-      state: eventMap[event.type],
-    };
-    console.log(payload);
-    conn.send(payload);
+  let conn = null;
+  const keys = [];
+  const areas = [];
+  const states = {};
+
+  function circlesIntersect(r1, r2, x1, y1, x2, y2) {
+    const dx = x2 - x1;
+    const dy = y2 - y1;
+    const radii = r1 + r2;
+    return (dx * dx + dy * dy < radii * radii);
   }
 
-  controller.contentDocument.addEventListener('touchstart', handleKey);
-  controller.contentDocument.addEventListener('touchend', handleKey);
-  controller.contentDocument.addEventListener('touchmove', event => {
+  function sendEvent(key, state) {
+    if (states[key] === state) {
+      return;
+    }
+    if (state) {
+      navigator.vibrate(50);
+    }
+
+    states[key] = state;
+    const payload = {
+      key,
+      state: state ? 'keydown' : 'keyup',
+    };
+    console.log('Sending update', payload);
+    if (conn) {
+      conn.send(payload);
+    }
+  }
+
+  function handleTouch(event) {
     event.preventDefault();
-  });
+    //console.log([...event.touches].map(touch => touch.radiusX));
+
+    const filter = {};
+    if (event.touches.length) {
+      [...event.touches].forEach(touch => {
+        areas.forEach(area => {
+          console.log(touch);
+          const intersects = circlesIntersect(area.radius, 12,
+            area.pos.x, area.pos.y, touch.clientX, touch.clientY);
+          filter[area.id] = filter[area.id] || intersects;
+        });
+      });
+    }
+    keys.forEach(key => sendEvent(key, filter[key] || false));
+  }
+
+  const svg = controller.contentDocument;
+  const touchables = svg.querySelectorAll('[id]');
+  for (let touchable, i = 0; touchable = touchables[i++];) {
+    const rect = touchable.getBoundingClientRect();
+    const area = {
+      id: touchable.id,
+      pos: {
+        x: rect.left + rect.width / 2,
+        y: rect.top + rect.height / 2,
+      },
+      radius: rect.width * .9,
+    };
+    areas.push(area);
+    keys.push(area.id);
+    states[area.id] = false;
+  }
+
+  svg.addEventListener('touchstart', handleTouch);
+  svg.addEventListener('touchend', handleTouch);
+  svg.addEventListener('touchmove', handleTouch);
 
   const id = window.location.hash.split('#')[1];
-  console.log(id);
-
-  const peer = new Peer({key: 'lwjd5qra8257b9'});
-  const conn = peer.connect(id);
-  conn.on('open', function() {
-    // Receive messages
-    conn.on('data', function(data) {
-      console.log('Received', data);
+  if (id) {
+    const peer = new Peer({key: 'lwjd5qra8257b9'});
+    conn = peer.connect(id);
+    conn.on('open', function() {
+      console.log('Connection established', conn);
+      conn.on('data', function(data) {
+        console.log('Received', data);
+      });
     });
-
-    // Send messages
-    conn.send('Hello!');
-  });
-
+  }
 });
