6e21cddfdf76bb0a31e2578530939cdbfe42508f
Pontus Persson <pom@spotify.com>
2016-08-26 14:22:41 +0200

    Add Tetris.

--- .modules/update.sh
+++ .modules/update.sh
@@ -6,3 +6,8 @@ git submodule update --recursive
 rm -rf "$DIR/../nes-remote" && \
    mkdir -p "$DIR/../nes-remote" && \
    cp -r "$DIR/nes-remote/src/"* "$DIR/../nes-remote/"
+
+# tetris
+rm -rf "$DIR/../tetris" && \
+   mkdir -p "$DIR/../tetris" && \
+   cp -r "$DIR/tetris/"* "$DIR/../tetris/"
